ef0f97777f55d06509caddcc59e0e7852d22bd20
Iain Rough <iain@cosource.dev>
2022-06-21 15:27:21 +0200

    Init

---
+++ TestPipeline/TestPipeline/ViewModels/BaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using Xamarin.Forms;
+
+using TestPipeline.Models;
+using TestPipeline.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TestPipeline.ViewModels
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+        public IDataStore<Item> DataStore => DependencyService.Get<IDataStore<Item>>();
+
+        [ObservableProperty]
+        private bool isBusy = false;
+
+        [ObservableProperty]
+        private string title = string.Empty;
+      
+    }
+}
+
